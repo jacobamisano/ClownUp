ea491421b212c7b237703649b7a4ac17ca724115
Jacob Amisano <jamisano2@huskers.unl.edu>
2022-09-16 20:49:29 -0500

    fixed issues within validateExistingADT design

--- AbstractDataTypes/ADTHelper.js
+++ AbstractDataTypes/ADTHelper.js
@@ -1,9 +1,9 @@
 import {assert} from '../Errors/Assert.js'
-export function validateExistingData(elements) {
+export function validateExistingADT(elements) {
   if (elements.length >= 0){
     const type = typeof elements[0];
-    for(i=1; i<elements.length; i++) {
-      assert(typeof elements[x] == type, `${elements[i]} at index ${i} does not match the required type ${type}`);
+    for(let i=1; i<elements.length; i++) {
+      assert(typeof elements[i] == type, `${elements[i]} at index ${i} does not match the required type ${type}`);
     }
     return true;
   }
@@ -12,7 +12,7 @@ export function validateExistingData(elements) {
   }
 }
 
-export function validateNewData(elements, newElement) {
+export function validateNewADTData(elements, newElement) {
   if (elements.length > 0){
     const type = typeof elements[0];
     assert((typeof newElement == type), `${newElement} does not have the required type of ${type} and cannot be added to the ADT`);
