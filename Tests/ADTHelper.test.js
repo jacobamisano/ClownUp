17e0def6e6e6c31b6a22d1d8af20c69fccca52b8
Jacob Amisano <jamisano2@huskers.unl.edu>
2022-09-16 20:47:58 -0500

    Updated ADTHelper tests to include validateExistingADT

--- Tests/ADTHelper.test.js
+++ Tests/ADTHelper.test.js
@@ -1,20 +1,20 @@
 import {describe, test, toBe} from '@jest/globals';
-import { validateNewData } from '../AbstractDataTypes/ADTHelper.js'
+import { validateNewADTData, validateExistingADT } from '../AbstractDataTypes/ADTHelper.js'
 import {assert} from '../Errors/Assert.js'
 import {NotEquivalentException} from '../Errors/Assert.js';
 
-describe('validateNewData Tests', () => {
-  test('validateNewData with correct data', () => {
+describe('validateNewADTData Tests', () => {
+  test('validateNewADTData with correct data', () => {
     const elements = [0, 1, 2];
     const newElement = 3;
-    assert(validateNewData(elements, newElement) == true, 'ValidateNewData failed with correct data');
+    assert(validateNewADTData(elements, newElement) == true, 'ValidateNewData failed with correct data');
   });
 
-  test('validateNewData with incorrect data', () => {
+  test('validateNewADTData with incorrect data', () => {
     const elements = [0, 1, 2];
     const newElement = "Not Right";
     try {
-      assert(validateNewData(elements, newElement) == null, 'ValidateNewData succeeded with incorrect data');
+      assert(validateNewADTData(elements, newElement) == null, 'ValidateNewData succeeded with incorrect data');
     } catch (e) {
       if (!(e instanceof NotEquivalentException)) {
         console.log(`${typeof e} is NOT a NotEquivalentException ${e}`)
@@ -22,4 +22,23 @@ describe('validateNewData Tests', () => {
       }
     }
   });
+})
+
+describe('validateExistingADT tests', () => {
+  test('validateExistingADT with correct inputs', () => {
+    const elements = [1, 2, 3];
+    assert(validateExistingADT(elements) == true, 'validateExistingADT failed with correct data');
+  })
+
+  test('validateExistingADT with incorrect inputs', () => {
+    const elements = [1, 2, "Not Right"];
+    try {
+      assert(validateExistingADT(elements), 'validateExistingADT succeeded with incorrect data');
+    } catch (e) {
+      if (!(e instanceof NotEquivalentException)) {
+        console.log(`${typeof e} is NOT a NotEquivalentException ${e}`)
+        throw 'Unexpected error when testing';
+      }
+    }
+  })
 })
